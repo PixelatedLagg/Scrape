75917e3cda66ddd1de9bac5f739a3f428934053f
PixelatedLagg <81400034+PixelatedLagg@users.noreply.github.com>
2021-10-08 23:51:40 -0400

    cleaned up main.c

--- ScrapeLang/Compiler/Main.c
+++ ScrapeLang/Compiler/Main.c
@@ -1,12 +1,8 @@
-/*
-Change Log:
-started store var to store a var of a give type (if you want to change the way we store vars please do)
-alot of errors to do with multilin char which is expected but i didnt have time to fix so please fix that
-also github repo is private for now until we have a decent product ready
-*/
-
 #include <stdio.h>
 #define FILE_EXTENSION .srp
+#define FILE_EXTENSION .scrape
+
+void storeVar(char, char);
 
 int Main()
 {
@@ -19,28 +15,18 @@ void storeVar(char Type, char Value)
     switch (Type)
     {
         case 'str':
-            // todo later
+            //add string
             break;
 
         case 'int': 
         {
+            //add integer
             break;
         }
-        /* Would throw a error for to many chars in const ammount
-        Potential fix would be to do a else if tree thing
-        case 'float': 
+        default: 
         {
-            break;
-        }
-
-        case 'double': 
-        {
-            break;
-        }
-        */
-
-        default: {
-            printf("error invalid type %s given", Type);
+            //error
+            printf("%s is not a valid type!", Type);
             break;
         }
     }
