dc3d50b8a5680a676f4c11d1e75c1f148643071a
PixelatedLagg <81400034+PixelatedLagg@users.noreply.github.com>
2022-02-06 17:20:23 -0500

    organized cli

--- ScrapeLang/Compiler/Program.cs
+++ ScrapeLang/Compiler/Program.cs
@@ -1,6 +1,5 @@
 using System;
 using System.IO;
-using Scrape.Cli;
 using Scrape.Code.Generation;
 
 namespace Scrape
@@ -10,42 +9,64 @@ namespace Scrape
         static void Main(string[] args) 
         {
             Global.Entrypoint = false;
-
-			if (args.Length < 1) {
-				Console.WriteLine("Usage: scrape <input>");
-
+			if (args.Length < 1) 
+            {
+				Console.WriteLine("Invalid command. Use <scrape help> to list all commands.");
 				return;
 			}
-
-			Console.WriteLine("Compiling");
-
-            foreach (string file in Directory.GetFiles(Path.Combine(Directory.GetCurrentDirectory(), args[0])))
+            switch (args[0])
             {
-                if (Path.GetExtension(file) == ".srp")
-                {
-                    LLVMCompiler compiler;
-
-                    using (StreamReader reader = new StreamReader(file)) 
+                case "run":
+                    foreach (string file in Directory.GetFiles(Directory.GetCurrentDirectory(), "*.srp", SearchOption.AllDirectories))
                     {
-                        compiler = new LLVMCompiler(reader.ReadToEnd());
+                        /*
+                        LLVMCompiler compiler;
+                        using (StreamReader reader = new StreamReader(file)) 
+                        {
+                            compiler = new LLVMCompiler(reader.ReadToEnd());
+                        }
+                        compiler.Compile();
+                        LLVMSharp.LLVM.DumpModule(compiler.Module);
+                        compiler.Compile();
+                        using (StreamWriter writer = new StreamWriter($"{Path.GetFileNameWithoutExtension(file)}.cpp")) 
+                        {
+                            writer.Write(compiler.Output);
+                        }
+                        */
                     }
-
-					compiler.Compile();
-
-					// LLVMSharp.LLVM.DumpModule(compiler.Module);
-
-                    /*compiler.Compile();
-
-                    using (StreamWriter writer = new StreamWriter($"{Path.GetFileNameWithoutExtension(file)}.cpp")) 
+                    break;
+                case "list":
+                    if (args.Length < 2)
                     {
-                        writer.Write(compiler.Output);
-                    }*/
-                }
-            }
-
-            if (!Global.Entrypoint)
-            {
-                throw new CompileError("no entrypoint provided!");
+                        Console.WriteLine("Must supply at least one file to compile.");
+                        return;
+                    }
+                    for (int x = 2; x < args.Length; x++)
+                    {
+                        if (Path.GetExtension(args[x]) != ".srp")
+                        {
+                            Console.WriteLine("Invalid file type.");
+                            return;
+                        }
+                        /*
+                        LLVMCompiler compiler;
+                        using (StreamReader reader = new StreamReader(args[x])) 
+                        {
+                            compiler = new LLVMCompiler(reader.ReadToEnd());
+                        }
+                        compiler.Compile();
+                        LLVMSharp.LLVM.DumpModule(compiler.Module);
+                        compiler.Compile();
+                        using (StreamWriter writer = new StreamWriter($"{Path.GetFileNameWithoutExtension(file)}.cpp")) 
+                        {
+                            writer.Write(compiler.Output);
+                        }
+                        */
+                    }
+                    break;
+                case "help":
+                    Console.WriteLine("Commands:^scrape run - Recursively search current directory and all sub directories to compile Scrape files.^scrape list <file(s)> - Compile all listed Scrape files.");
+                    break;
             }
 		}
     }
